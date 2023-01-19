ca7165689a08a2a3103964c4daa7abbf53f84c2a
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-01-19 21:51:27 +0200

    add task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Знаходимо список по id
+const listEl = document.querySelector("#ingredients");
+// Створюю масив в який буду пушити список li
+const liArray = [];
+// перебираємо за допомогою методу forEach
+ingredients.forEach((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.className = "item";
+  itemEl.textContent = ingredient;
+  liArray.push(itemEl);
+});
+
+listEl.append(...liArray);
