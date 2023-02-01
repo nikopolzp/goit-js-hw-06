0cb811b405f861793fe11f1b920b55fe0569d777
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-01 18:08:51 +0200

    add solution task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxes = document.querySelector("#boxes");
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxes.appendChild(box);
+
+    width += 10;
+    height += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxes = document.querySelector("#boxes");
+  boxes.innerHTML = "";
+}
+
+const controls = document.querySelector("#controls");
+controls.addEventListener("click", (event) => {
+  if (event.target.matches("[data-create]")) {
+    const input = controls.querySelector("input");
+    createBoxes(input.value);
+  } else if (event.target.matches("[data-destroy]")) {
+    destroyBoxes();
+  }
+});
