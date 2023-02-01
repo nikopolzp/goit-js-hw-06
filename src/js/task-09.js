bf6dc93fdd85b28bec2520566e0ee732899f89f5
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-01 13:17:16 +0200

    add solution task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector("button.change-color");
+const span = document.querySelector("span.color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+button.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.innerHTML = randomColor;
+});
