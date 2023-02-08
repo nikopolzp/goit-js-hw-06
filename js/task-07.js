a204857841ce652e6719fd8c5c22d551a7f9ca8e
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-09 01:02:28 +0200

    work with mentor's comments

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputChange = document.querySelector("input#font-size-control");
 const spanTextEl = document.querySelector("span#text");
 
-inputChange.addEventListener("change", (e) => {
+inputChange.addEventListener("input", (e) => {
   spanTextEl.style.fontSize = `${e.target.value}px`;
 });
